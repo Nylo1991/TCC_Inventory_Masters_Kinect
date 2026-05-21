ecd9221b878a290416ed82df0bec6e06e4dda153
Danilo <nylo.dk@gmail.com>
2026-05-21 20:16:49 -0300

    Implementação da Clsse MedicaoVolume

--- TCC_Inventory_Masters_Kinect/Model/MedicaoVolume.cs
+++ TCC_Inventory_Masters_Kinect/Model/MedicaoVolume.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace TCC_Inventory_Masters_Kinect.Model
 {
-    internal class MedicaoVolume
+    public class MedicaoVolume
     {
+        public int Id { get; set; }
+        public double VolumeCm3 { get; set; }
+
+        public DateTime DataHora { get; set; }
+
+        public bool KinectLigado { get; set; }
+
+        public bool Calibrado { get; set; }
+
+        public string Status { get; set; }
     }
 }
