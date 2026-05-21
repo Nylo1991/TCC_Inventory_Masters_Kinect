cdafa9a63ca52cb7b60eeeea9aaf07b3cb907e4f
Diulie <diuliemileide7@gmail.com>
2026-05-21 20:18:27 -0300

    Implemetação da classe IKinectRepository.

--- TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
+++ TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCC_Inventory_Masters_Kinect.Model;
 
 namespace TCC_Inventory_Masters_Kinect.Repository.Interface
 {
-    internal class IKinectRepository
+    public interface IKinectRepository
     {
+        void SalvarMedicao(MedicaoVolume medicao);
     }
 }
