bac97f45c217699b928765838a26b822c0912e37
Danilo <nylo.dk@gmail.com>
2026-05-21 23:15:54 -0300

    implementação da Migrations

---
+++ TCC_Inventory_Masters_Kinect/Migrations/202605220205506_CriacaoInicial.cs
@@ -0,0 +1,30 @@
+﻿namespace TCC_Inventory_Masters_Kinect.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CriacaoInicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MedicaoVolumes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        VolumeCm3 = c.Double(nullable: false),
+                        DataHora = c.DateTime(nullable: false),
+                        KinectLigado = c.Boolean(nullable: false),
+                        Calibrado = c.Boolean(nullable: false),
+                        Status = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.MedicaoVolumes");
+        }
+    }
+}
