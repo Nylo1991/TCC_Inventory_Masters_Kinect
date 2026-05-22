10bc5dd35e4049d0e6df83c39e6d3039391f67c9
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-21 21:26:36 -0300

    Declarando a estrutura do banco

--- TCC_Inventory_Masters_Kinect/Data/AppDbContext.cs
+++ TCC_Inventory_Masters_Kinect/Data/AppDbContext.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using InventoryMastersKinect.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
+using TCC_Inventory_Masters_Kinect.Model;
 
-namespace TCC_Inventory_Masters_Kinect.Data
+namespace InventoryMastersKinect.Data
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<MedicaoVolume> MedicoesVolume { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // O arquivo do banco será gerado automaticamente na raiz do projeto
+            optionsBuilder.UseSqlite("Data Source=inventory_masters.db");
+        }
     }
 }
