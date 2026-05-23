92680c20a6fff3dd78e2c349e8b01f148619d6d1
Diulie <diuliemileide7@gmail.com>
2026-05-22 21:56:18 -0300

    alteração da Dbcontext

--- TCC_Inventory_Masters_Kinect/Data/AppDbContext.cs
+++ TCC_Inventory_Masters_Kinect/Data/AppDbContext.cs
@@ -1,18 +1,29 @@
 ﻿using System.Data.Entity;
 using TCC_Inventory_Masters_Kinect.Model;
+using System.Data.SQLite.EF6;
 
 namespace TCC_Inventory_Masters_Kinect.Data
 {
+    // Esta linha diz ao Entity Framework como traduzir os comandos para o SQLite
+    [DbConfigurationType(typeof(SQLiteConfigurationInternal))]
     public class AppDbContext : DbContext
     {
-        // O nome "InventoryMastersDb" liga este código ao App.config
         public AppDbContext() : base("name=InventoryMastersDb")
         {
-            // Estratégia para SQLite: Se o banco não existir, ele cria automaticamente
-            // com base nas classes DbSet (como MedicaoVolume)
             Database.SetInitializer(new CreateDatabaseIfNotExists<AppDbContext>());
         }
 
         public DbSet<MedicaoVolume> MedicoesVolume { get; set; }
+    }
+
+    // Classe de configuração interna (não precisa de arquivo novo)
+    public class SQLiteConfigurationInternal : DbConfiguration
+    {
+        public SQLiteConfigurationInternal()
+        {
+            SetProviderFactory("System.Data.SQLite", System.Data.SQLite.SQLiteFactory.Instance);
+            SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
+            SetProviderServices("System.Data.SQLite", (System.Data.Entity.Core.Common.DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(System.Data.Entity.Core.Common.DbProviderServices)));
+        }
     }
 }
