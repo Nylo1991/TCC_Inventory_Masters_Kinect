b8f2ee00ea8dee867d1be67b84f3e474830e83f6
Diulie <diuliemileide7@gmail.com>
2026-05-24 19:27:56 -0300

    implemetação do log e do app xaml cs

--- TCC_Inventory_Masters_Kinect/App.xaml.cs
+++ TCC_Inventory_Masters_Kinect/App.xaml.cs
@@ -1,40 +1,26 @@
-﻿using System;
-using System.Windows;
-using TCC_Inventory_Masters_Kinect.Data;
+﻿using System.Windows;
+using TCC_Inventory_Masters_Kinect.Service;
 
 namespace TCC_Inventory_Masters_Kinect
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(
+            StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            try
-            {
-                using (var db = new AppDbContext())
-                {
-                    db.Database.CreateIfNotExists();
+            LoggerService.Log(
+                "Sistema iniciado.");
+        }
 
-                    db.Database.ExecuteSqlCommand(@"
-                        CREATE TABLE IF NOT EXISTS MedicaoVolumes (
-                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            VolumeCm3 REAL NOT NULL,
-                            DataHora DATETIME NOT NULL,
-                            KinectLigado INTEGER NOT NULL,
-                            Calibrado INTEGER NOT NULL,
-                            Status TEXT
-                        );
-                    ");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString(), "ERRO SQLITE");
-            }
+        protected override void OnExit(
+            ExitEventArgs e)
+        {
+            LoggerService.Log(
+                "Sistema encerrado.");
 
-            MainWindow janela = new MainWindow();
-            janela.Show();
+            base.OnExit(e);
         }
     }
 }
