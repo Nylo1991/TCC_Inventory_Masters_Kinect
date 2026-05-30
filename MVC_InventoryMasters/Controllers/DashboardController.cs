be8750d936a538249c04ebc81796d7a566416904
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-30 18:08:53 -0300

    Implementação da classe DashboardViewModel.cs

---
+++ MVC_InventoryMasters/Controllers/DashboardController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_InventoryMasters.Repositories;
+
+namespace MVC_InventoryMasters.Controllers
+{    
+    public class DashboardController : Controller
+    {
+        private readonly MedicaoVolumeRepository _medicaoRepo;
+        private readonly NotificacaoRepository _notificacaoRepo;
+        private readonly ParceirosRepository _parceirosRepo;
+        private readonly ParametrosSistemaRepository _parametrosRepo;
+
+        // Injeção dos 4 repositórios no construtor
+        public DashboardController(
+            MedicaoVolumeRepository medicaoRepo,
+            NotificacaoRepository notificacaoRepo,
+            ParceirosRepository parceirosRepo,
+            ParametrosSistemaRepository parametrosRepo)
+        {
+            _medicaoRepo = medicaoRepo;
+            _notificacaoRepo = notificacaoRepo;
+            _parceirosRepo = parceirosRepo;
+            _parametrosRepo = parametrosRepo;
+        }
+
+        // Ação para exibir o dashboard, onde vamos buscar os dados
+        // de todos os repositórios e enviar para a view
+        public IActionResult Index()
+        {
+            // Cria um objeto "ViewModel" anônimo para enviar tudo de uma vez para a view
+            var viewModel = new
+            {
+                Medicoes = _medicaoRepo.ListarTodas(),
+                Alertas = _notificacaoRepo.ListarHistorico(),
+                Parceiros = _parceirosRepo.ListarTodos(),
+                Parametros = _parametrosRepo.Buscar()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MVC_InventoryMasters/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVC_InventoryMasters.Models;
+using System.Collections.Generic;
+
+namespace MVC_InventoryMasters.ViewModels
+{
+    // Herda de BaseViewModel para ter acesso aos recursos de notificação de mudança
+    public class DashboardViewModel : BaseViewModel
+    {
+        public List<MedicaoVolume> Medicoes { get; set; } = new List<MedicaoVolume>();
+        public List<Notificacao> Alertas { get; set; } = new List<Notificacao>();
+        public List<Parceiro> Parceiros { get; set; } = new List<Parceiro>();
+        public ParametrosSistema Parametros { get; set; } = new ParametrosSistema();
+
+        // Adiciona uma propriedade para mensagens de erro, caso o Firebase falhe
+        public string MensagemErro { get; set; }
+    }
+}
