943dc6e4d741e80bda4f43cb7ed8cac649f4dd80
Diulie <diuliemileide7@gmail.com>
2026-05-29 21:28:23 -0300

    alteração na IKinectRepository para recebimento das novas funcionalidades do sistema .

--- TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
+++ TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
@@ -4,6 +4,22 @@ namespace TCC_Inventory_Masters_Kinect.Repository.Interface
 {
     public interface IKinectRepository
     {
-        void SalvarMedicao(MedicaoVolume medicao);
+        void SalvarMedicao(
+            MedicaoVolume medicao);
+
+        void SalvarEspaco(
+            EspacoMapeado espaco);
+
+        void SalvarHistorico(
+            HistoricoOcupacao historico);
+
+        void SalvarSnapshot(
+            SnapshotEspacial snapshot);
+
+        EspacoMapeado ObterEspaco(
+            int id);
+
+        EspacoMapeado ObterEspacoPorNome(
+            string nomeEspaco);
     }
 }
