6369642dfa444b6c72515da09deede64cb6804bf
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-30 14:39:25 -0300

    Implementação na Model - Classe Notificacao.

--- MVC_InventoryMasters/Models/Notificacao.cs
+++ MVC_InventoryMasters/Models/Notificacao.cs
@@ -1,6 +1,21 @@
-﻿namespace MVC_InventoryMasters.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_InventoryMasters.Models
 {
     public class Notificacao
     {
+        [Key] 
+        public int Id { get; set; }
+        
+        public int MedicaoId { get; set; }
+        
+        public int ParceiroId { get; set; }
+        
+        public DateTime DataEnvio { get; set; }
+        
+        public string? StatusEnvio { get; set; }
+        
+        public string? Mensagem { get; set; }
     }
 }
