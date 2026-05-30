e9c52f1d16fe11b3d7e1a846163032690913b92b
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-30 14:43:45 -0300

    Implementação do ParametrosSistema na Model.

--- MVC_InventoryMasters/Models/ParametrosSistema.cs
+++ MVC_InventoryMasters/Models/ParametrosSistema.cs
@@ -1,6 +1,22 @@
-﻿namespace MVC_InventoryMasters.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_InventoryMasters.Models
 {
     public class ParametrosSistema
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string NomeParametro { get; set; } // Ex: "VolumeMaximoEstoque"
+
+        [Required]
+        public double ValorMaximo { get; set; }   // Valor limite para gatilho de alerta
+
+        public double ValorMinimo { get; set; }   // Opcional: limite mínimo, se necessário
+
+        public string UnidadeMedida { get; set; } // Ex: "cm3"
+
+        public DateTime DataAtualizacao { get; set; }
     }
 }
