cbe5cd50eb044c3d6fc28b1246714ce69da9e3b7
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-30 16:36:21 -0300

    Implementação da classe MedicaoVolumeRepository

---
+++ MVC_InventoryMasters/Repositories/MedicaoVolumeRepository.cs
@@ -0,0 +1,41 @@
+﻿using Google.Cloud.Firestore;
+using MVC_InventoryMasters.Models;
+using System.Collections.Generic;
+
+namespace MVC_InventoryMasters.Repositories
+{
+    public class MedicaoVolumeRepository
+    {        
+        private readonly string _colecao = "Medicoes";
+        private readonly FirestoreDb _db;
+
+        public MedicaoVolumeRepository(string projectId)
+        {
+            _db = FirestoreDb.Create(projectId);
+        }
+
+        public List<MedicaoVolume> ListarTodas()
+        {
+            List<MedicaoVolume> lista = new List<MedicaoVolume>();
+
+            // Obtém todos os documentos da coleção "Medicoes"
+            var documentos = _db.Collection(_colecao).GetSnapshotAsync().Result;
+
+            // Converte cada documento para o modelo MedicaoVolume e adiciona à lista
+            foreach (var doc in documentos.Documents)
+            {
+                MedicaoVolume m = doc.ConvertTo<MedicaoVolume>();                
+
+                lista.Add(m);
+            }
+
+            return lista;
+        }
+
+        public void Adicionar(MedicaoVolume medicao)
+        {
+            // Adiciona um novo documento à coleção "Medicoes" com as medições vindas do kinect
+            _db.Collection(_colecao).AddAsync(medicao).Wait();
+        }
+    }
+}
