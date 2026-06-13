27f353af676102879d5a6d236b8f70080ab6c006
Diulie <diuliemileide7@gmail.com>
2026-06-13 12:30:39 -0300

    alteração da kinet repository para tirrar resquisio de codigo com uso do 3 d e implmentação de logger  e teirando parte do codigo que expunha erros

--- TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
+++ TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
@@ -5,11 +5,9 @@ namespace TCC_Inventory_Masters_Kinect.Repository.Interface
 {
     public interface IKinectRepository
     {
-     
         void SalvarMedicao(MedicaoVolume medicao);
         List<MedicaoVolume> ObterUltimasMedicoes(int quantidade);
 
-      
         void SalvarHistorico(HistoricoOcupacao historico);
         List<HistoricoOcupacao> ObterHistoricoPorEspaco(int espacoId);
         List<HistoricoOcupacao> ObterUltimosHistoricos(int quantidade);
