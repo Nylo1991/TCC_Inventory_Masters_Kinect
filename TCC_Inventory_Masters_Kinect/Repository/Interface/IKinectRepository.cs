2d0a6e2dd945a1b77adb6c4bd33b7fefafe62f57
Danilo <nylo.dk@gmail.com>
2026-06-14 14:08:37 -0300

    padroniza acesso a medições e históricos de ocupação"

--- TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
+++ TCC_Inventory_Masters_Kinect/Repository/Interface/IKinectRepository.cs
@@ -6,10 +6,11 @@ namespace TCC_Inventory_Masters_Kinect.Repository.Interface
     public interface IKinectRepository
     {
         void SalvarMedicao(MedicaoVolume medicao);
-        List<MedicaoVolume> ObterUltimasMedicoes(int quantidade);
+          List<MedicaoVolume> ObterUltimasMedicoes(int quantidade);
+          List<MedicaoVolume> ObterMedicoesEmOrdemCrescente(int quantidade);
 
         void SalvarHistorico(HistoricoOcupacao historico);
-        List<HistoricoOcupacao> ObterHistoricoPorEspaco(int espacoId);
-        List<HistoricoOcupacao> ObterUltimosHistoricos(int quantidade);
+          List<HistoricoOcupacao> ObterHistoricoPorEspaco(int espacoId);
+          List<HistoricoOcupacao> ObterUltimosHistoricos(int quantidade);
     }
 }
