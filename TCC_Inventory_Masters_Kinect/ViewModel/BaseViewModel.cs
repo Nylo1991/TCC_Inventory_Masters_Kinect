fc8e8101fdfcb81f55f6fdf4584cc95fa12600d8
Diulie <diuliemileide7@gmail.com>
2026-06-14 22:33:19 -0300

    Documentação da class BaseViewModel

--- TCC_Inventory_Masters_Kinect/ViewModel/BaseViewModel.cs
+++ TCC_Inventory_Masters_Kinect/ViewModel/BaseViewModel.cs
@@ -3,8 +3,17 @@ using System.Runtime.CompilerServices;
 
 namespace TCC_Inventory_Masters_Kinect.ViewModel
 {
+
+    /// <summary>
+    /// resposável por fornecer a funcionalidade básica de notificação de propriedade para os ViewModels da aplicação,
+    /// permitindo que a interface do usuário seja atualizada automaticamente quando os dados subjacentes mudam.
+    /// </summary>
     public class BaseViewModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Evento que é acionado quando uma propriedade é alterada, permitindo que a interface do usuário seja notificada
+        /// sobre a mudança.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
@@ -15,7 +24,11 @@ namespace TCC_Inventory_Masters_Kinect.ViewModel
             OnPropertyChanged(propertyName);
             return true;
         }
-
+        /// <summary>
+        /// Método protegido que dispara o evento PropertyChanged para notificar a 
+        /// interface do usuário sobre mudanças em uma propriedade.
+        /// </summary>
+        /// <param name="propertyName">Nome da propriedade que mudou.</param>
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
