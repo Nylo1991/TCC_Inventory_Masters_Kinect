a7bd6963f952a336689639944bbced02bc5af90a
Diulie <diuliemileide7@gmail.com>
2026-06-15 19:20:56 -0300

    Documentação da class HistoricoMedicoesWindow

--- TCC_Inventory_Masters_Kinect/View/HistoricoMedicoesWindowxaml.xaml.cs
+++ TCC_Inventory_Masters_Kinect/View/HistoricoMedicoesWindowxaml.xaml.cs
@@ -7,6 +7,9 @@ namespace TCC_Inventory_Masters_Kinect.View
 {
     public partial class HistoricoMedicoesWindow : Window
     {
+        /// <summary>
+        /// classe responsave por exixbir o historico de medição em tempo real .
+        /// </summary>
         private readonly MainViewModel _mainViewModel;
         private readonly DispatcherTimer _timer;
 
@@ -31,13 +34,23 @@ namespace TCC_Inventory_Masters_Kinect.View
 
             _timer.Start();
         }
-
+        /// <summary>
+        /// método de fechar page de historico de mediação.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Fechar_Click(object sender, RoutedEventArgs e)
         {
             _timer.Stop();
             Close();
         }
 
+        /// <summary>
+        /// método de onclosed foi implmentando  para que  para que o sistema não busque dados
+        /// desnecessarios ,assegurando o ciclo de vida da tela, ao realizar o  fechamento a tela o sistema da stop 
+        /// e para de buscar dados  não sobrecarregando a memoria ram .
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnClosed(EventArgs e)
         {
             _timer.Stop();
