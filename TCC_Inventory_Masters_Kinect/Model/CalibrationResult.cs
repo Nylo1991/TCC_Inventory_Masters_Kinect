60f1414a8501f6515c237a2aa1f2316e683e9ab9
Diulie <diuliemileide7@gmail.com>
2026-06-14 22:35:38 -0300

    Documentação da class CalibrationResult

--- TCC_Inventory_Masters_Kinect/Model/CalibrationResult.cs
+++ TCC_Inventory_Masters_Kinect/Model/CalibrationResult.cs
@@ -2,6 +2,10 @@
 
 namespace TCC_Inventory_Masters_Kinect.Model
 {
+    /// <summary>
+    /// class Calibration Result é usada para armazenar os resultados da calibração do Kinect,
+    /// incluindo o volume máximo detectado,o número total de pontos encontrados e a data/hora da calibração.
+    /// </summary>
     public class CalibrationResult
     {
         public double MaxVolume { get; set; }
