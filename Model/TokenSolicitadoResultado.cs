be7bbd3b2a4e2c6164768f12eddd14bb27c649e1
DiulieBatista <diuliemileide7@gmail.com>
2026-06-25 19:32:15 -0300

    Delete Model directory

--- Model/TokenSolicitadoResultado.cs
+++
@@ -1,9 +0,0 @@
-namespace TCC_Inventory_Masters_Kinect.Model
-{
-    public class TokenSolicitadoResultado
-    {
-        public bool Sucesso { get; set; }
-        public string Email { get; set; }
-        public string Mensagem { get; set; }
-    }
-}
